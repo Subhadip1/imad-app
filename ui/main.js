0906c4dcd3efd37fd39f46314e836e41d284dc4a
Subhadip Chakraborty <subhadip1sc@gmail.com>
2017-08-14 01:03:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,13 @@ var element = document.getElementById('main-text');
 element.innerHTML="New Value";
 //Move the Image On Click
 var img = document.getElementById('madi');
+var marginLeft = 0;
+function(moveRight)
+{
+    marginLeft= marginLeft +10;
+    img.style.marginLeft=marginLeft +'px';
+}
 img.onclick= function()
 {
-    img.style.marginLeft="200px";
+    var interval = setInterval(moveRight,20);
 }
