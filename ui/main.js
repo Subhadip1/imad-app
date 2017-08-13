331778d931ecf3ea67cf01305b561e0a98f2f8b6
Subhadip Chakraborty <subhadip1sc@gmail.com>
2017-08-14 00:54:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,9 @@ console.log('Loaded!');
 //Change the text of the index.html
 var element = document.getElementById('main-text');
 element.innerHTML="New Value";
+//Move the Image On Click
+var img = document.getElementById('madi');
+img.onclick= function()
+{
+    img.style.marginLeft="50px";
+}
