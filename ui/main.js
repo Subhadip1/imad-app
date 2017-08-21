aeb7b15a5329875741a23ec423aea6490480a958
Subhadip Chakraborty <subhadip1sc@gmail.com>
2017-08-21 16:55:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,28 @@
 //counter
 
 var button= document.getElementById("counter");
-var counter= 0;
 button.onclick = function(){
     
-    //Make a response to the server on our click request
+    //Create a request object
+    var request = new XMLHttpRequest();
     
-    //Store response in a variable
+    //Capture response and store it in a variable
     
-    //Render the variable in correct Span
-    
-    counter = counter+1;
-    var span= document.getElementById("count");
-    span.innerHTML = counter.toString(); 
-}
+    request.onreadystatechange = function(){
+        
+        if(request.readyState === XMLHttpRequest.DONE){
+            
+            //Take some action
+            if(requeststatus=== 200){
+                
+                var counter = request.responseText;
+                var span= document.getElementById("count");
+                span.innerHTML = counter.toString(); 
+            }
+        }
+    // Not done Yet
+    };
+    //Make Request
+    request.open('GET',"http://subhadip1sc.imad.hasura-app.io/counter",true);
+    request.send(null);
+};
